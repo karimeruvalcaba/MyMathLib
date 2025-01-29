dfe31ba75ce0ec8bc6dba8c517a6aa7154eb3b5b
Karime Itzel Ruvalcaba Pérez <137795975+karimeruvalcaba@users.noreply.github.com>
2025-01-29 00:59:27 +0000

    pruebita3

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
-#include "MyMathLib.h"
+#include "../MyMathLib.h"  // Ensure correct relative path from 'tests/' folder
 
 TEST(MathTestCase, MathTestSqrt9)
 {
-	ASSERT_EQ(3, mySqrt(9));
+    ASSERT_EQ(3, mySqrt(9));
 }
