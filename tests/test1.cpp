6ec2558404c1aca711ebc4259ba1399a95bb7d31
Karime Itzel Ruvalcaba Pérez <137795975+karimeruvalcaba@users.noreply.github.com>
2025-01-29 19:07:05 -0600

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -3,35 +3,30 @@
 
 using namespace std; 
 
-// NOSONAR: GTest macros do not follow lowercase naming convention
 TEST(KarimeMathLibTest, SumTest)
 {
     KarimeMathLib mathLib;
     ASSERT_EQ(mathLib.my_sum(5, 5), 10);
 }
 
-// NOSONAR: GTest macros do not follow lowercase naming convention
 TEST(KarimeMathLibTest, DifferenceTest)
 {
     KarimeMathLib mathLib;
     ASSERT_EQ(mathLib.my_difference(10, 5), 5);
 }
 
-// NOSONAR: GTest macros do not follow lowercase naming convention
 TEST(KarimeMathLibTest, MultiplicationTest)
 {
     KarimeMathLib mathLib;
     ASSERT_EQ(mathLib.my_multiplication(5, 5), 25);
 }
 
-// NOSONAR: GTest macros do not follow lowercase naming convention
 TEST(KarimeMathLibTest, DivisionTest)
 {
     KarimeMathLib mathLib;
     ASSERT_EQ(mathLib.my_division(5, 5), 1);
 }
 
-// NOSONAR: GTest macros do not follow lowercase naming convention
 TEST(KarimeMathLibTest, DivisionByZero)
 {
     KarimeMathLib mathLib;
