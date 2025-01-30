779270723c388f9c9513463214b7386b1e5f2d04
Karime Itzel Ruvalcaba Pérez <137795975+karimeruvalcaba@users.noreply.github.com>
2025-01-29 19:03:47 -0600

    Update test2.cpp

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -1,24 +1,28 @@
 #include <gtest/gtest.h>
 #include "../KarimeMathLib2.h" 
 
+// NOSONAR: GTest macros do not follow lowercase naming convention
 TEST(KarimeMathLib2Test, FactorialTest)
 {
     KarimeMathLib2 mathLib2;
     ASSERT_EQ(mathLib2.my_factorial(5), 120);
 }
 
+// NOSONAR: GTest macros do not follow lowercase naming convention
 TEST(KarimeMathLib2Test, CombinationTest)
 {
     KarimeMathLib2 mathLib2;
     ASSERT_EQ(mathLib2.my_combination(5, 2), 10);
 }
 
+// NOSONAR: GTest macros do not follow lowercase naming convention
 TEST(KarimeMathLib2Test, FactorialNegative)
 {
     KarimeMathLib2 mathLib2;
     ASSERT_THROW(mathLib2.my_factorial(-1), invalid_argument);
 }
 
+// NOSONAR: GTest macros do not follow lowercase naming convention
 TEST(KarimeMathLib2Test, CombinationInvalid)
 {
     KarimeMathLib2 mathLib2;
